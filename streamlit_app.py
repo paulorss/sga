cd116c2de1484c2993b4be80bb6f54f551a440a6
paulorss <61366907+paulorss@users.noreply.github.com>
2025-01-10 12:37:08 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,7 @@ import hydralit_components as hc
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 from io import BytesIO
+import pytz
 
 st.set_page_config(
     page_title="SGA - Senhas",
@@ -72,7 +73,7 @@ def create_csv_if_not_exists(csv_file):
     if not os.path.exists(csv_file):
         with open(csv_file, 'w', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(['id', 'senha', 'tipo', 'serviço', 'nome', 'data', 'hora', 'atendido'])
+            writer.writerow(['id', 'senha', 'tipo', 'serviço', 'nome', 'data', 'hora', 'atendido', 'guiche'])
 
 def generate_password(prefix):
     return f"{prefix}{random.randint(1, 999):03d}"
@@ -83,7 +84,7 @@ def add_to_queue(csv_file, senha, tipo, servico, nome):
     hora = now.strftime("%H:%M:%S")
     with open(csv_file, 'a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow([get_next_id(csv_file), senha, tipo, servico, nome, data, hora, 0])
+        writer.writerow([get_next_id(csv_file), senha, tipo, servico, nome, data, hora, 0, ''])
 
 def get_next_id(csv_file):
     with open(csv_file, 'r') as file:
@@ -101,32 +102,39 @@ def display_queue(csv_file, queue_type):
     else:
         st.error("Fila vazia.")
 
-# Atualize também a função call_next_password
-def call_next_password(csv_file, company):
+def call_next_password(csv_file, company, selected_service, counter_number):
     with open(csv_file, 'r') as file:
         reader = csv.reader(file)
         rows = list(reader)
     
-    prioritario = next((row for row in rows if row[2] == 'Prioritário' and row[7] == '0'), None)
-    geral = next((row for row in rows if row[2] == 'Geral' and row[7] == '0'), None)
+    # First try to find a priority ticket for the selected service
+    prioritario = next((row for row in rows if row[2] == 'Prioritário' and 
+                       row[3] == selected_service and row[7] == '0'), None)
+    
+    # If no priority ticket, look for a general ticket
+    geral = next((row for row in rows if row[2] == 'Geral' and 
+                 row[3] == selected_service and row[7] == '0'), None)
     
     row = prioritario or geral
     
     if row:
-        row[7] = '1'  # Marcar como atendido
+        row[7] = '1'  # Mark as attended
+        row[8] = str(counter_number)  # Add counter number
         with open(csv_file, 'w', newline='') as file:
             writer = csv.writer(file)
             writer.writerows(rows)
-        update_last_called(company, row[1], row[4], row[2])  # row[4] agora é 'nome' em vez de 'cpf_cnpj'
-        return row[1], row[4], row[2]
+        update_last_called(company, row[1], row[4], row[2], counter_number, selected_service)
+        return row[1], row[4], row[2], counter_number, selected_service
     else:
-        return None, None, None
+        return None, None, None, None, None
 
-def update_last_called(company, senha, nome, tipo):
+def update_last_called(company, senha, nome, tipo, counter, service):
     data = {
         'senha': senha,
-        'nome': nome,  # Alterado de 'cpf_cnpj' para 'nome'
+        'nome': nome,
         'tipo': tipo,
+        'guiche': counter,
+        'servico': service,
         'timestamp': time.time()
     }
     file_path = os.path.join(COMPANIES_DIR, f'{company}_last_called.json')
@@ -138,47 +146,27 @@ def get_last_called_from_file(company):
     if os.path.exists(file_path):
         with open(file_path, 'r') as f:
             data = json.load(f)
-        return data.get('senha'), data.get('nome'), data.get('tipo'), data.get('timestamp', 0)
-    return None, None, None, 0
-
-
-def get_last_called_password(csv_file):
-    with open(csv_file, 'r') as file:
-        reader = csv.reader(file)
-        rows = list(reader)
-    
-    for row in reversed(rows[1:]):  # Ignorar o cabeçalho e começar do final
-        if row[7] == '1':  # Se foi atendido
-            return row[1], row[4], row[2]  # Retorna a senha, o nome e o tipo
-    
-    return None, None, None
-
-import pytz
-
+        return (
+            data.get('senha'),
+            data.get('nome'),
+            data.get('tipo'),
+            data.get('timestamp', 0),
+            data.get('guiche', ''),
+            data.get('servico', '')
+        )
+    return None, None, None, 0, '', ''
 
 def generate_password_pdf_in_memory(senha, tipo, nome, servico, company):
-    # Criar um buffer de bytes para gerar o PDF em memória
     pdf_buffer = BytesIO()
-    
-    # Inicializar o canvas com o buffer
     c = canvas.Canvas(pdf_buffer, pagesize=letter)
-    
-    # Definir a fonte (use fontes padrão, como Helvetica)
     c.setFont("Helvetica-Bold", 16)
     
-    # Obter a data e hora atual
-    # Definir o fuso horário de São Paulo
     fuso_horario = pytz.timezone('America/Sao_Paulo')
-
-    # Obter a hora atual no fuso horário de São Paulo
     now = datetime.datetime.now(fuso_horario)
-
-    # Formatar a data e hora
     date_time = now.strftime("%d/%m/%Y %H:%M:%S")
 
-    # Adicionar conteúdo ao PDF na ordem especificada
-    y_position = 750  # Posição inicial do topo
-    line_height = 25  # Altura entre as linhas
+    y_position = 750
+    line_height = 25
 
     c.drawString(100, y_position, f"Empresa: {company}")
     y_position -= line_height
@@ -197,32 +185,24 @@ def generate_password_pdf_in_memory(senha, tipo, nome, servico, company):
 
     c.drawString(100, y_position, f"Data e hora de Geração da senha: {date_time}")
 
-    # Finalizar o PDF
     c.showPage()
     c.save()
-
-    # Mover o ponteiro de leitura para o início do buffer
     pdf_buffer.seek(0)
-
-    # Retornar o PDF como bytes
     return pdf_buffer.getvalue()
 
 def get_pdf_download_link(pdf_data):
-    # Use the bytes buffer directly
-    b64 = base64.b64encode(pdf_data).decode()  # Encode in base64
+    b64 = base64.b64encode(pdf_data).decode()
     href = f'<a href="data:application/pdf;base64,{b64}" download="senha.pdf">Baixar Senha PDF</a>'
     return href
 
 def add_to_queue_and_generate_pdf(csv_file, senha, tipo, servico, nome, company):
-    # Adicionar à fila
     now = datetime.datetime.now()
     data = now.strftime("%d/%m/%Y")
     hora = now.strftime("%H:%M:%S")
     with open(csv_file, 'a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow([get_next_id(csv_file), senha, tipo, servico, nome, data, hora, 0])
+        writer.writerow([get_next_id(csv_file), senha, tipo, servico, nome, data, hora, 0, ''])
     
-    # Gerar PDF
     pdf_buffer = generate_password_pdf_in_memory(senha, tipo, nome, servico, company)
     return pdf_buffer
 
@@ -259,7 +239,6 @@ def generate_password_html(senha, tipo, nome, servico, company):
     """
     return html_content
 
-
 def main_app():
     csv_file = get_csv_file(st.session_state.company)
     create_csv_if_not_exists(csv_file)
@@ -270,21 +249,25 @@ def main_app():
         st.session_state.update_counter = 0
 
     st_autorefresh(interval=500, key=f"autorefresh_{st.session_state.update_counter}")
-    if 'last_called' not in st.session_state:
-        st.session_state.last_called = get_last_called_password(csv_file)
-
+    
+    # Update this section to handle all returned values
+    senha, nome, tipo, timestamp, guiche, servico = get_last_called_from_file(st.session_state.company)
+    
     if 'last_update' not in st.session_state:
         st.session_state.last_update = 0
     
-    senha, nome, tipo, timestamp = get_last_called_from_file(st.session_state.company)
     if timestamp > st.session_state.last_update:
-        st.session_state.last_called = (senha, nome, tipo)
+        st.session_state.last_called = (senha, nome, tipo, guiche, servico)
         st.session_state.last_update = timestamp
         st.rerun()
 
+    servicos_disponiveis = ["Registro de Imóveis", "Registro Civil Pessoas Naturais", 
+                           "Tabelionato de Notas", "Protesto", 
+                           "Registro de Títulos e Documentos", 
+                           "Registro Civil Pessoas Jurídicas"]
+
     tab1, tab2, tab3 = st.tabs(["Cadastramento e Acionamento", "Painel de Chamada", "Auto Atendimento"])
 
-    # Add this JavaScript function to your Streamlit app
     st.markdown("""
     <script>
     function openPrintWindow(htmlContent) {
@@ -302,8 +285,6 @@ def main_app():
             st.subheader("Emitir Nova Senha")
             nome = st.text_input("Nome do Cliente", key="nome_cliente_tab1")
             queue_type = st.radio("Tipo de Atendimento", ["Geral", "Prioritário"], key="queue_type_tab1")
-            
-            servicos_disponiveis = ["Registro de Imóveis", "Registro Civil Pessoas Naturais", "Tabelionato de Notas", "Protesto", "Registro de Títulos e Documentos", "Registro Civil Pessoas Jurídicas"]
             servico = st.selectbox("Escolha o Serviço", servicos_disponiveis, key="servico_tab1")
 
             if 'pdf_data' not in st.session_state:
@@ -312,18 +293,37 @@ def main_app():
             if st.button("Gerar Senha", key="gerar_senha_tab1"):
                 if nome:
                     senha = generate_password("G" if queue_type == "Geral" else "P")
-                    st.session_state.pdf_data = add_to_queue_and_generate_pdf(csv_file, senha, queue_type, servico, nome, st.session_state.company)
+                    st.session_state.pdf_data = add_to_queue_and_generate_pdf(
+                        csv_file, senha, queue_type, servico, nome, st.session_state.company
+                    )
                     st.success(f"Senha gerada: {senha}")
                 else:
                     st.error("Por favor, insira o nome do cliente.")
 
-            # Display PDF download link if pdf_data exists
             if st.session_state.pdf_data is not None:
                 pdf_download_link = get_pdf_download_link(st.session_state.pdf_data)
                 st.markdown(pdf_download_link, unsafe_allow_html=True)
 
         with col2:
-            st.subheader("Filas de Atendimento")
+            st.subheader("Chamar Próxima Senha")
+            
+            selected_service = st.selectbox(
+                "Selecione o Serviço para Chamar",
+                servicos_disponiveis,
+                key="service_selection"
+            )
+            
+            counter_number = st.number_input(
+                "Número do Guichê",
+                min_value=1,
+                max_value=99,
+                value=1,
+                step=1,
+                key="counter_number"
+            )
+            
+            st.divider()
+            
             st.success("Fila Geral:")
             display_queue(csv_file, "Geral")
             st.divider()
@@ -332,32 +332,40 @@ def main_app():
             display_queue(csv_file, "Prioritário")
             
             if st.button("Chamar Próxima Senha"):
-                senha, nome, tipo = call_next_password(csv_file, st.session_state.company)
+                senha, nome, tipo, guiche, servico = call_next_password(
+                    csv_file, 
+                    st.session_state.company,
+                    selected_service,
+                    counter_number
+                )
                 if senha and nome:
-                    st.session_state.last_called = (senha, nome, tipo)
+                    st.session_state.last_called = (senha, nome, tipo, guiche, servico)
                     st.success(f"Senha chamada: {senha} - Usuário: {nome} - Tipo: {tipo}")
+                    st.success(f"Dirija-se ao Guichê {guiche} - Serviço: {servico}")
                     st.session_state.update_counter += 1
                     st.rerun()
                 else:
-                    st.info("Não há senhas na fila.")
+                    st.info(f"Não há senhas na fila para o serviço: {selected_service}")
 
     with tab2:
         st.subheader("Senha Chamada")
         last_called_placeholder = st.empty()
         last_five_placeholder = st.empty()
 
-        senha, nome, tipo = st.session_state.last_called
-
         with last_called_placeholder.container():
             if senha and nome:
-                theme_good = {'bgcolor': '#EFF8F7','title_color': 'green','content_color': 'green','icon_color': 'green', 'icon': 'fa fa-check-circle'}
+                theme_good = {'bgcolor': '#EFF8F7','title_color': 'green',
+                             'content_color': 'green','icon_color': 'green', 
+                             'icon': 'fa fa-check-circle'}
                 
-                hc.info_card(title=f"Senha: {senha} ({tipo})",  
-                             content=f"Usuário: {nome}", 
-                             sentiment='good',
-                             bar_value=100,
-                             theme_override=theme_good,
-                             key=f"last_called_{senha}_{st.session_state.update_counter}")
+                hc.info_card(
+                    title=f"Senha: {senha} ({tipo})",  
+                    content=f"Usuário: {nome}\nGuichê: {guiche}\nServiço: {servico}", 
+                    sentiment='good',
+                    bar_value=100,
+                    theme_override=theme_good,
+                    key=f"last_called_{senha}_{st.session_state.update_counter}"
+                )
             else:
                 st.info("Nenhuma senha chamada ainda.")
         
@@ -369,15 +377,19 @@ def main_app():
             
             called_passwords = [row for row in reversed(rows) if row[7] == '1'][:5]  # Pegando as últimas 5 senhas chamadas
             
-            theme_neutral = {'bgcolor': '#f9f9f9','title_color': 'orange','content_color': 'orange','progress_color': 'orange','icon_color': 'orange', 'icon': 'fa fa-question-circle'}
+            theme_neutral = {'bgcolor': '#f9f9f9','title_color': 'orange',
+                           'content_color': 'orange','progress_color': 'orange',
+                           'icon_color': 'orange', 'icon': 'fa fa-question-circle'}
             
             for i, row in enumerate(called_passwords):
-                hc.info_card(title=f"Senha: {row[1]} ({row[2]})",
-                            content=f"Usuário: {row[4]}", 
-                            sentiment='neutral',
-                            theme_override=theme_neutral,
-                            bar_value=100,
-                            key=f"last_five_{i}_{row[1]}_{st.session_state.update_counter}")
+                hc.info_card(
+                    title=f"Senha: {row[1]} ({row[2]})",
+                    content=f"Usuário: {row[4]}\nGuichê: {row[8]}\nServiço: {row[3]}", 
+                    sentiment='neutral',
+                    theme_override=theme_neutral,
+                    bar_value=100,
+                    key=f"last_five_{i}_{row[1]}_{st.session_state.update_counter}"
+                )
 
     with tab3:
         st.subheader("Auto Atendimento")
